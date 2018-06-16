f3cbb146872a9fd197de5b540875ad8d1d87645c
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-06-16 14:50:21 -0600

    Build with different FFT size options

--- arm.cmake
+++ arm.cmake
@@ -26,12 +26,44 @@ set(SOS_LIB_SOURCELIST ${SOURCELIST} ${CMAKE_SOURCE_DIR}/arm_dsp_api.h ${CMAKE_S
 set(SOS_LIB_TYPE release)
 set(SOS_LIB_INCLUDE_DIRECTORIES ${SOURCE_DIR}/../Include ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
 set(SOS_LIB_OPTION kernel)
-install(DIRECTORY ${SOURCE_DIR}/../Include/ DESTINATION ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
-install(FILES ${CMAKE_SOURCE_DIR}/arm_dsp_api.h ${CMAKE_SOURCE_DIR}/arm_dsp_api_declaration.h DESTINATION include)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=65536)
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
+
+# Linking with the kernel -- FFT size limits 512
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_INCLUDE_DIRECTORIES ${SOURCE_DIR}/../Include ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
+set(SOS_LIB_OPTION kernel_fft_512)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=512)
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
+
+# Linking with the kernel -- FFT size limits 256
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_INCLUDE_DIRECTORIES ${SOURCE_DIR}/../Include ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
+set(SOS_LIB_OPTION kernel_fft_256)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=256)
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
+
+# Linking with the kernel -- FFT size limits 128
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_INCLUDE_DIRECTORIES ${SOURCE_DIR}/../Include ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
+set(SOS_LIB_OPTION kernel_fft_128)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=128)
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
+
+# Linking with the kernel -- FFT size limits 64
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_INCLUDE_DIRECTORIES ${SOURCE_DIR}/../Include ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
+set(SOS_LIB_OPTION kernel_fft_64)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=64)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
 
 # Link within an app
 set(SOS_LIB_OPTION "")
 set(SOS_LIB_TYPE release)
 set(SOS_LIB_BUILD_FLAGS -mlong-calls)
+set(SOS_LIB_DEFINITIONS -DSOS_ARM_DSP_FFT_MAX_SIZE=65536)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
+
+install(DIRECTORY ${SOURCE_DIR}/../Include/ DESTINATION ${TOOLCHAIN_INC_DIR}/mcu/arch/cmsis)
+install(FILES ${CMAKE_SOURCE_DIR}/arm_dsp_api.h ${CMAKE_SOURCE_DIR}/arm_dsp_api_declaration.h DESTINATION include)
+
